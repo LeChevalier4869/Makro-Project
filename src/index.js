3cf1d2c2998d0a973756aa810dc98e99eef98e66
Le Chevalier <assawinknw48@gmail.com>
2024-01-23 09:22:51 +0700

    thrid upload

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-require('dotenv').config()
+require('dotenv').config()              //-- env บรรทัดแรกเสมอ
 
-const express = require('express');
+const express = require('express');    //-- import express (webserver)
 const multer = require('multer');
 const cors = require('cors')
-const errorHandler = require('../middlewares/error');
-const notFoundHandler = require('../middlewares/notFound');
-const authRoute = require('../routes/auth-route');
-const productRoute = require('../routes/product-route');
+const errorHandler = require('./middlewares/error');
+const notFoundHandler = require('./middlewares/notFound');
+const authRoute = require('./routes/auth-route');
+const productRoute = require('./routes/product-route');
 const app = express();
 const port = process.env.PORT
 
-app.use(express());
-app.use(cors());
+//app.use(express());
+app.use(express.json());
+app.use(cors());                        //-- ข้าม port, domain
 app.use(errorHandler);
 app.use('*', notFoundHandler);
 app.use('/product', productRoute);
 app.use('/auth', authRoute);
 
-app.listen(port, () => {
+app.listen(port, () => {    //-- รัน web server
     console.log(`Server is running on port ${port}`);
 });
 
 //--------------------------------------------------------------
+
+app.get('/', () => {});
